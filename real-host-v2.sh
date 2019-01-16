0a715951d314ca4d6bd7c378a27acc21baf626ad
RealStrategy <36258164+RealStrategy@users.noreply.github.com>
2019-01-16 11:25:16 -0500

    Update real-host-v2.sh

--- real-host-v2.sh
+++ real-host-v2.sh
@@ -92,7 +92,7 @@ read foo
 8)echo ""
 echo -e "\e[1;33mCREDITS TO THE DEVELOPER\e[0m"
 echo ""
-echo -e "\e[1;31mCoder: @RealStrategy"
+echo -e "\e[1;31mCoder: @RealStrategy y el grupo vip RS"
 echo -e "\e[1;32m"
 echo "YOUTUBE : https://youtube.com/RealHackRWAM "
 echo "TELEGRAM: https://t.me/RealHackRWAM "
